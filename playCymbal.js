494db02759015393b72df68dc84a1bedcca38e90
Kapooflord <omaristhenoob@gmail.com>
2025-02-14 06:24:48 -0500

    sup.
    sup.

---
+++ playCymbal.js
@@ -0,0 +1,21 @@
+function playCymbal() {
+    document.addEventListener("keydown", function (event) {
+      if (event.key === "f" || event.key === "F") {
+        var cymbal = document.getElementById("cymbal");
+        cymbal.setAttribute("src", "dependencies/crashcymbaldown.png");
+        new Audio("dependencies/crashcymbal.wav").play();
+        setTimeout(function () {
+          cymbal.setAttribute("src", "dependencies/crashcymbalup.png");
+        }, 100);
+      }
+    });
+  }
+  
+  function clickCymbal() {
+    var cymbal = document.getElementById("cymbal");
+    cymbal.setAttribute("src", "dependencies/crashcymbaldown.png");
+    new Audio("dependencies/crashcymbal.wav").play();
+    setTimeout(function () {
+      cymbal.setAttribute("src", "dependencies/crashcymbalup.png");
+    }, 100);
+  }

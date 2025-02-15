2380b5442940edd6c08714089d31d5fee9108be3
Nathaniel Moy <145233084+SnowyNate@users.noreply.github.com>
2025-02-15 15:03:23 -0500

    Rename index.html and fix scripts

--- dependencies/scripts/playSnare.js
+++ dependencies/scripts/playSnare.js
@@ -2,10 +2,10 @@ function playSnare() {
   document.addEventListener("keydown", function (event) {
     if (event.key === "k" || event.key === "K") {
       var snare = document.getElementById("snare");
-      snare.setAttribute("src", "dependencies/snaredrumdown.png");
-      new Audio("dependencies/snare.mp3").play();
+      snare.setAttribute("src", "dependencies/objects/snaredrum/snaredrumdown.png");
+      new Audio("dependencies/sounds/snare.mp3").play();
       setTimeout(function () {
-        snare.setAttribute("src", "dependencies/snaredrumup.png");
+        snare.setAttribute("src", "dependencies/objects/snaredrum/snaredrumup.png");
       }, 100);
     }
   });
@@ -13,9 +13,9 @@ function playSnare() {
 
 function clickSnare() {
   var snare = document.getElementById("snare");
-  snare.setAttribute("src", "dependencies/snaredrumdown.png");
-  new Audio("dependencies/snare.mp3").play();
+  snare.setAttribute("src", "dependencies/objects/snaredrum/snaredrumdown.png");
+  new Audio("dependencies/sounds/snare.mp3").play();
   setTimeout(function () {
-    snare.setAttribute("src", "dependencies/snaredrumup.png");
+    snare.setAttribute("src", "dependencies/objects/snaredrum/snaredrumup.png");
   }, 100);
 }
